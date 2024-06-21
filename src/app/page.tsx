1c2e5530d088596ba197c63bc89cd5f52b2f1a82
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-06-21 13:39:16 +0100

    adding skelton squares to search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,22 @@
+import Album from '@/components/album';
 import Search from '@/components/search';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row items-center justify-between">
-      <div className="w-1/3 min-h-screen p-4 border-solid border-0 border-r border-primary flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-center pb-10">Top Album Grid</h1>
-        <Search />
-      </div>
-      <div className="w-2/3 min-h-screen flex flex-row items-center justify-center px-16 py-8">
-        <div className="grid grid-cols-5 gap-4 h-full">
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
-          <div className="border h-32 w-32 border-primary"></div>
+    <main className="max-w-screen-xl min-h-screen m-auto">
+      <div className="flex min-h-screen flex-row items-center justify-between">
+        <div className="flex-none min-w-[416px] min-h-screen border-solid border-0 border-r border-primary flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-bold text-center pb-10">
+            Top Album Grid
+          </h1>
+          <Search />
+        </div>
+        <div className="flex-grow min-h-screen flex flex-row items-center justify-center py-8">
+          <div className="grid grid-cols-5 gap-2 h-full">
+            {Array.from({ length: 25 }).map((_, index) => (
+              <Album key={index} />
+            ))}
+          </div>
         </div>
       </div>
     </main>
---
+++ src/components/album.tsx
@@ -0,0 +1,7 @@
+function Album() {
+  // TODO: add props for border, img, and alt
+
+  return <div className="border h-32 w-32 border-primary"></div>;
+}
+
+export default Album;
