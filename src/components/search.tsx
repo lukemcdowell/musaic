d379b6b79dbf7ba178d726daf04ae18b1dd96704
Luke McDowell <luke.db.mcdowell@gmail.com>
2024-06-21 13:39:16 +0100

    adding skelton squares to search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,12 +2,17 @@
 
 import { useState } from 'react';
 import { Input } from './ui/input';
+import { Skeleton } from './ui/skeleton';
 
 function Search() {
   const [results, setResults] = useState<any[] | null>([]);
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<Boolean>(false);
 
   const handleSearch = async (searchTerm: String) => {
+    setResults(null);
+    setLoading(true);
+
     if (searchTerm === null || searchTerm.trim() === '') {
       setResults(null);
     } else {
@@ -24,7 +29,26 @@ function Search() {
         setError('An error occurred while fetching data');
       }
     }
+
+    setLoading(false);
   };
+
+  const renderEmptyDivs = () =>
+    Array.from({ length: 9 }).map((_, index) => (
+      <div key={index} className="border h-32 w-32 border-primary"></div>
+    ));
+
+  const renderAlbums = () =>
+    results?.map((album) => (
+      <div key={album.id} className="border h-32 w-32">
+        <img src={album.images[0]?.url} alt={album.name} />
+      </div>
+    ));
+
+  const renderSkeletons = () =>
+    Array.from({ length: 9 }).map((_, index) => (
+      <Skeleton key={index} className="h-32 w-32" />
+    ));
 
   return (
     <div className="w-full px-5">
@@ -39,11 +63,10 @@ function Search() {
       {error ?? <p>{error}</p>}
 
       <div className="grid grid-cols-3 gap-4 h-full py-4">
-        {results?.map((album) => (
-          <div key={album.id} className="border h-32 w-32 border-primary">
-            <img src={album.images[0]?.url} alt={album.name} />
-          </div>
-        ))}
+        {(results === null || results.length === 0) && !loading
+          ? renderEmptyDivs()
+          : renderAlbums()}
+        {loading && renderSkeletons()}
       </div>
     </div>
   );
