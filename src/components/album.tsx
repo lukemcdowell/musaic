58ab8996caa2b1fc474d3afe0efa09ad45d8224c
Luke McDowell <luke.db.mcdowell@gmail.com>
2024-07-05 20:45:22 +0100

    making album component more generic, adding mock data

--- src/components/album.tsx
+++ src/components/album.tsx
@@ -2,18 +2,21 @@ import { joinArtists } from '@/lib/utils';
 import { AlbumType } from '@/types/types';
 
 interface AlbumProps {
-  album: AlbumType;
+  album?: AlbumType | null;
+  key?: number;
 }
 
 function Album({ album }: AlbumProps) {
-  return (
-    <div key={album.id} className="border h-32 w-32">
+  return album ? (
+    <div className="border h-32 w-32">
       <img
         src={album.images[0]?.url}
         alt={album.name}
         title={`${album.name} - ${joinArtists(album.artists)}`}
       />
     </div>
+  ) : (
+    <div className="border h-32 w-32 border-primary"></div>
   );
 }
 
