99eeb772d01c7a38d40a3ae77d58bae53683ae33
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-07-14 15:08:40 +0100

    reverting to using img

--- src/components/result-album.tsx
+++ src/components/result-album.tsx
@@ -1,7 +1,6 @@
 import { SQUARE_DIMENSIONS } from '@/constants/constants';
 import { joinArtists } from '@/lib/utils';
 import { Album } from '@/types/types';
-import Image from 'next/image';
 
 interface ResultAlbumProps {
   album?: Album | null;
@@ -14,7 +13,7 @@ function ResultAlbum({ album, onClick }: ResultAlbumProps) {
       className={`border ${SQUARE_DIMENSIONS} rounded cursor-pointer`}
       onClick={onClick}
     >
-      <Image
+      <img
         src={album.images[0]?.url}
         alt={album.name}
         title={`${album.name} - ${joinArtists(album.artists)}`}
