02f0ca267f341c82e6a27f0ee01ced20717a188a
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-07-14 13:53:22 +0100

    adding download button loading state and toast messages

--- src/components/download-button.tsx
+++ src/components/download-button.tsx
@@ -1,5 +1,7 @@
-import { Download } from 'lucide-react';
+import { Download, Loader2 } from 'lucide-react';
+import { useState } from 'react';
 import { Button } from './ui/button';
+import { useToast } from './ui/use-toast';
 
 interface DownloadButtonProps {
   imageUrls: string[];
@@ -7,8 +9,11 @@ interface DownloadButtonProps {
 }
 
 function DownloadButton({ imageUrls, gridFull }: DownloadButtonProps) {
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+
   const handleDownloadCollage = async () => {
-    console.log('download button clicked');
+    setLoading(true);
 
     try {
       const response = await fetch('/api/collage', {
@@ -30,18 +35,28 @@ function DownloadButton({ imageUrls, gridFull }: DownloadButtonProps) {
       a.download = 'musaic.png';
       a.click();
       window.URL.revokeObjectURL(url);
+
+      toast({
+        description: `Musaic downloaded successfully`,
+      });
     } catch (error) {
       console.error('Error downloading collage:', error);
+      toast({
+        variant: 'destructive',
+        title: 'There was a problem downloading your Musaic',
+        description: 'Please refresh and try again',
+      });
     }
+    setLoading(false);
   };
 
   return (
     <Button
       variant="outline"
       onClick={handleDownloadCollage}
-      disabled={!gridFull}
+      disabled={!gridFull || loading}
     >
-      <Download />
+      {loading ? <Loader2 className="animate-spin" /> : <Download />}
     </Button>
   );
 }
