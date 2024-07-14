8f5bb344b581f87413c8aaff125af001247572a2
Luke McDowell <luke.db.mcdowell@gmail.com>
2024-07-14 14:53:23 +0100

    dynamically hitting auth endpoint from request baseUrl

--- src/components/info-dialog.tsx
+++ src/components/info-dialog.tsx
@@ -2,7 +2,7 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogHeader,
+  DialogTitle,
 } from '@/components/ui/dialog';
 
 interface InfoDialogProps {
@@ -18,48 +18,48 @@ function InfoDialog({ openModal, setOpenModal }: InfoDialogProps) {
   return (
     <Dialog open={openModal} onOpenChange={closeModal}>
       <DialogContent className="w-full sm:w-max">
-        <DialogHeader>
-          <DialogDescription>
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left">
-              About
-            </h3>
-            <p className="mt-2 mb-4 text-left">
-              Musaic showcases your favourite music visually.
-            </p>
+        <DialogTitle>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            About
+          </h1>
+        </DialogTitle>
+        <DialogDescription>
+          <p className="mb-4 text-left">
+            Musaic showcases your favourite music visually.
+          </p>
 
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left">
-              How to Use
-            </h3>
-            <ul className="mt-2 mb-4 ml-6 list-disc [&>li]:mt-2 marker:text-primary text-left">
-              <li>
-                <strong className="text-primary">Add an Album:</strong> Click on
-                a square to search for an album and click on the album cover to
-                add it to the grid.
-              </li>
-              <li>
-                <strong className="text-primary">Add Multiple Albums:</strong>{' '}
-                Use the "Add Albums" button to populate the grid with multiple
-                albums.
-              </li>
-              <li>
-                <strong className="text-primary">Clear Grid:</strong> Use the
-                "Clear Grid" button to reset the grid.
-              </li>
-              <li>
-                <strong className="text-primary">Download Grid:</strong> Use the
-                "Download As Image" button to download your grid as an image.
-                You can only download a full grid of 20 albums.
-              </li>
-            </ul>
-
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left">
-              Data Source
-            </h3>
-            <p className="my-2 text-left">
-              All music data is fetched in real-time from the Spotify API.
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left">
+            How to Use
+          </h3>
+          <div className="mt-2 mb-4 ml-4 [&>p]:mt-2 marker:text-primary text-left">
+            <p>
+              <strong className="text-primary">- Add an Album:</strong> Click on
+              a square to search for an album and click on the album cover to
+              add it to the grid.
             </p>
-          </DialogDescription>
-        </DialogHeader>
+            <p>
+              <strong className="text-primary">- Add Multiple Albums:</strong>{' '}
+              Use the "Add Albums" button to populate the grid with multiple
+              albums.
+            </p>
+            <p>
+              <strong className="text-primary">- Clear Grid:</strong> Use the
+              "Clear Grid" button to reset the grid.
+            </p>
+            <p>
+              <strong className="text-primary">- Download Grid:</strong> Use the
+              "Download As Image" button to download your grid as an image. You
+              can only download a full grid of 20 albums.
+            </p>
+          </div>
+
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-left">
+            Data Source
+          </h3>
+          <p className="my-2 text-left">
+            All music data is fetched in real-time from the Spotify API.
+          </p>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
