ce9b4ca74b7c32663201fa3b00e46f10cf39f8ce
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-08-01 20:04:18 +0100

    adding scrolling for mobile

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -3,6 +3,12 @@ import { useCallback } from 'react';
 import { isMobile } from 'react-device-detect';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import withScrolling, {
+  BoxType,
+  createHorizontalStrength,
+  createVerticalStrength,
+  Point,
+} from 'react-dnd-scrolling';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import TopAlbum from './top-album';
 
@@ -13,6 +19,28 @@ interface GridProps {
 }
 
 function Grid({ topAlbums, setTopAlbums, handleAlbumClick }: GridProps) {
+  const ScrollingComponent = withScrolling('div');
+
+  const linearHorizontalStrength = createHorizontalStrength(150);
+  const linearVerticalStrength = createVerticalStrength(150);
+
+  // this easing function is from https://gist.github.com/gre/1650294 and
+  // expects/returns a number between [0, 1], however strength functions
+  // expects/returns a value between [-1, 1]
+  function ease(val: number) {
+    const t = (val + 1) / 2; // [-1, 1] -> [0, 1]
+    const easedT = t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    return easedT * 2 - 1; // [0, 1] -> [-1, 1]
+  }
+
+  function hStrength(box: BoxType, point: Point) {
+    return ease(linearHorizontalStrength(box, point));
+  }
+
+  function vStrength(box: BoxType, point: Point) {
+    return ease(linearVerticalStrength(box, point));
+  }
+
   const moveAlbum = useCallback(
     (dragIndex: number, hoverIndex: number) => {
       const updatedAlbums = [...topAlbums];
@@ -28,7 +56,11 @@ function Grid({ topAlbums, setTopAlbums, handleAlbumClick }: GridProps) {
       backend={isMobile ? TouchBackend : HTML5Backend}
       options={isMobile ? { enableMouseEvents: true } : ''}
     >
-      <div className="grid gap-2 h-full grid-cols-2 sm:grid-cols-4 lg:grid-cols-5">
+      <ScrollingComponent
+        className="grid gap-2 h-full grid-cols-2 sm:grid-cols-4 lg:grid-cols-5"
+        verticalStrength={vStrength}
+        horizontalStrength={hStrength}
+      >
         {topAlbums.map((album, index) => (
           <TopAlbum
             key={index}
@@ -38,7 +70,7 @@ function Grid({ topAlbums, setTopAlbums, handleAlbumClick }: GridProps) {
             moveAlbum={moveAlbum}
           />
         ))}
-      </div>
+      </ScrollingComponent>
     </DndProvider>
   );
 }
