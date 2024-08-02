8900f7df3c00781039f761e56177c851c5e14549
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-08-02 17:52:05 +0100

    setting up jest testing

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,12 @@
+// import { render, screen } from '@testing-library/react';
+// import Home from '../src/app/page';
+
+describe('Home', () => {
+  it('renders the title', () => {
+    // render(<Home />);
+    // const title = screen.getByText('Musaic');
+    // expect(title).toBeInTheDocument();
+
+    expect(true).toBeTruthy();
+  });
+});
