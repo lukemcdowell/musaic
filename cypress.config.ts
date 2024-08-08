3855f815286a597d3c6103a41b7098773e54970f
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-08-08 13:31:50 +0100

    adding task to clean up cypress download folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'cypress';
+import { removeDownloadsFolder } from './cypress/support/tasks';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on('task', { removeDownloadsFolder });
+      return config;
+    },
   },
 });
